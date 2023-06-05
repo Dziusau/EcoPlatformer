5321f3aa172165768cd902680e6cedb84d89a268
Aleksandr Dyusov <a_dyusov@wargaming.net>
2023-06-05 17:58:51 +0300

    fixed save system

--- Assets/_Scripts/SaveSystem.cs
+++ Assets/_Scripts/SaveSystem.cs
@@ -8,12 +8,35 @@ public static class SaveSystem
     public static string path = Application.persistentDataPath + "players.data";
      public static void SavePlayer(string _level, int _score, float _time)
     {
+        List<PlayerData> playerDataList = LoadPlayersData();
+        PlayerData updatedData = new(_level, _score, _time);
+
+        if (playerDataList != null)
+        {
+            // Find the index of the existing data for the same level, if it exists
+            int existingIndex = playerDataList.FindIndex(data => data.levelName == _level);
+
+            if (existingIndex != -1)
+            {
+                // Replace the existing data with the updated data
+                playerDataList[existingIndex] = updatedData;
+            }
+            else
+            {
+                // Add the new data to the list
+                playerDataList.Add(updatedData);
+            }
+        }
+        else
+        {
+            // Create a new list with the updated data if no existing data is found
+            playerDataList = new List<PlayerData>() { updatedData };
+        }
+
         BinaryFormatter formatter = new BinaryFormatter();
-        FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+        FileStream stream = new FileStream(path, FileMode.Create);
 
-        PlayerData playerData = new PlayerData(_level, _score, _time);
-
-        formatter.Serialize(stream, playerData);
+        formatter.Serialize(stream, playerDataList);
         stream.Close();
     }  
 
